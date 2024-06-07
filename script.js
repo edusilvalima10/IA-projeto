bde47d9561894448a37db8e2649d5c00a329f18e
estudante alura <113905804+edusilvalima10@users.noreply.github.com>
2024-06-07 08:49:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const caixaPrincipal = document.querySelectorAll(".caixa principal");
+const caixaPerguntas = document.querySelectorAll(".caixa perguntas");
+const caixaAlternativas = document.querySelectorAll(".caixa alternativas");
+const caixaResultado = document.querySelectorAll(".caixa resultado");
+const textoResultado = document.querySelectorAll(".resultado final");
+
+const perguntas = [
+    {
+    enunciado: "pergunta",
+    alternativas:[
+        "alternativa1",
+        "alternativa2"
+]
+},
+
+{
+enunciado: "pergunta2",
+alternativas:[
+    "alternativa1",
+    "alternativa2"
+]
+},        
+{
+enunciado: "pergunta3",
+alternativas:[
+    "alternativa1",
+    "alternativa2"
+]
+}
+
+
+]
