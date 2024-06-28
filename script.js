fee3751977e8c1913a7faf6e5490a2354abdfcdd
estudante alura <113905804+edusilvalima10@users.noreply.github.com>
2024-06-28 08:55:39 -0300

    Add files via upload
    escrevi duas das perguntas

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const textoResultado = document.querySelectorAll(".resultado final");
 
 const perguntas = [
     {
-    enunciado: "pergunta",
+    enunciado: "Você é um milionário e filamtropo de extrema importância para sua cidade e nação. Certo dia você recebe uma ligação de uma autoridade anônima lhe dizendo: Não me perguntre quem sou e de onde estou ligando, apenas saiba que suspeitamos que algo grande está para acontecer. Precisamos de sua ajuda, está dentro?",
     alternativas:[
-        "alternativa1",
-        "alternativa2"
+        "Estou dentro",
+        "Estou fora"
 ]
 },
 
 {
-enunciado: "pergunta2",
+enunciado: "Após receber detalhes a repeito de uma reunião ultra secreta, você se dirige a uma sede do governo onde descobre que até mesmo o presidente está no local. de costas, o presidente diz: A Venezuela comunicou diversas nações a respeito de um a tecnologia de IA que ultrapassa todas as já existentes, mas ao analisarmos percebemos que há algo de estranho nessa história, pois o país não tem a estrutura e nem o investimento necessário para criar algo tão inovador. São os detalhes quie precisa saber, queremos que vá numa missão ao país para investigar o que está ocorrendo. O pesidente se vira para vocẽ e pergunta: Você irá? ",
 alternativas:[
-    "alternativa1",
-    "alternativa2"
+    "Irei.",
+    "Não correrei tal risco."
 ]
 },        
 {
