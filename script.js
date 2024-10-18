c4ee8c4a682ac06f15d24e7b7ea4a1df4632d238
estudante alura <113905804+edusilvalima10@users.noreply.github.com>
2024-10-18 08:08:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,43 @@
-const caixaPrincipal = document.querySelectorAll(".caixa principal");
-const caixaPerguntas = document.querySelectorAll(".caixa perguntas");
-const caixaAlternativas = document.querySelectorAll(".caixa alternativas");
-const caixaResultado = document.querySelectorAll(".caixa resultado");
-const textoResultado = document.querySelectorAll(".resultado final");
+:root{
+    --cor-fundo: #01080E ;
+    --cor-principal: #0B0D20 ;
+    --cor-secundaria: #212333; 
+    --cor-destaque: #2BDEFD;
+    --cor-texto: #D7F9FF;
 
-const Perguntas = [
-    {
-    enunciado: "Você é um milionário e filamtropo de extrema importância para sua cidade e nação. Certo dia você recebe uma ligação de uma autoridade anônima lhe dizendo: Não me perguntre quem sou e de onde estou ligando, apenas saiba que suspeitamos que algo grande está para acontecer. Precisamos de sua ajuda, está dentro?",
-    alternativas:[
-
-        {
-        texto: "Estou dentro",
-        },
-        {
-        afirmação: 
-
-        },
-
-        {
-       texto: "Estou fora"  
-        },
-        {
-       afirmação: 
-
-        }
-]
-},
-
-{
-enunciado: "Após receber detalhes a repeito de uma reunião ultra secreta, você se dirige a uma sede do governo onde descobre que até mesmo o presidente está no local. De costas, o presidente diz: A Venezuela comunicou diversas nações a respeito de um a tecnologia de IA que ultrapassa todas as já existentes, mas ao analisarmos percebemos que há algo de estranho nessa história, pois o país não tem a estrutura e nem o investimento necessário para criar algo tão inovador. São os detalhes que precisa saber, queremos que você nos ajude cedendo algumas de suas tecnologias avançadas e usar seu dinheiro para deixar tudo por debaixo dos panos, tanto para as nação es quanto para a população de nosso país. Um espião será enviado numa missão ao país para investigar o que está ocorrendo. O presidente se vira para vocẽ e pergunta: Você ajudará? ",
-alternativas:[
-    {
-    texto: "certamente.",
-    }, 
-    {
-    afirmação: "Isso é loucura."
-    }
-]
-},        
-{
-enunciado: "O espião chega ao país e descobre o local onde está sendo desenvolvido a inteligência artificial. Lá ele observa um grupo de homens terno entregando uma maleta om um objeto brilhante dentro. Ele segue a maleta e eles chegam a uma sala com um robô gigante de aparência nada amigável. Eles inserem o objeto no peito do robô que inicia e ergue a cabeça. O espião se posiciona num ponto estratégico nos ferros de sustentação da estrutura da sala e nos afirma carrega uma bomba pequena em seu cinturão, ele a pega na mão, mira no peito da máquina e pergunta: Devo destruir?",
-alternativas:[
-    {
-    texto: "Jogue, isso ja foi longe demais! Devemos intervir antes que algo ocorra!"
-    },
-    
-    {
-    afirmação:
-
-    },
-
-    {
-    texto: "Aguarde, precisamos de mais infirmações!"
-    }, 
-    {
-    afirmação: ""
-    }
-]
 }
 
-
-]
-
-let atual = 0;
-let perguntaAtual;
-
-function mostraPergunta(){
-perguntaAtual = perguntas[atual];
-caixaPerguntas.textContent = perguntaAtual.enunciado;
-mostraAlternativas();
+body {
+    background-color: var(--cor-fundo);
+    color: var(--cor-texto);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-    const botãoAlternativas = document.createElement("button");
-    botãoAlternativa.textContent = alternativa;
-    caixaAlternativas.appendChild(botãoAlternativas);
+
+.caixa-principal{
+    background-color: var(--cor-principal);
+    width: 90%;
+    max-width: 600px;
+    text-align: center;
+    padding: 20px;
 }
+
+h1{
+    color: var(--cor-destaque);
 }
-mostraPergunta()
+
+button {
+    background-color: var(--cor-secundaria);
+    color: var(--cor-texto);
+    border: none;
+    border-radius: 15px;
+    padding: 15px;
+    transition: background-color 0.3s;
+}
+
+button:hover{
+    background-color: var(--cor-destaque);
+    color: var(--cor-principal);
+}
